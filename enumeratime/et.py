3aa907526cc3a086bcadb234a7ecf94d091f0031
Michael Ruppert <mr@atr0x.com>
2019-06-05 11:25:35 +0200

    energysave

--- enumeratime/et.py
+++ enumeratime/et.py
@@ -28,6 +28,7 @@ class EnumeraTIME:
         except:
             pass
         self.laststamp = time.time()
+        self.kstamp = time.time()
     def __iter__(self):
         self.t1=time.time()
         self.n=0
@@ -47,14 +48,16 @@ class EnumeraTIME:
                 except:
                     pass
                 self.laststamp = tstamp
-            tmt=tstamp-self.t1
-            now=time.localtime(self.t1+tmt*(1/(self.n/self.l)))
-            now = time.strftime("%H:%M \033[0m%d.%m.%Y",now)
-            #print("="*5)
-            print(" \033[34m{:.1%}\033[0m".format(percent), end=" ")
-            print("ETA: %5.1fs\033[0m"%(tmt*(1/(self.n/self.l))-tmt), end=" ",sep="")
-            print("\033[32m",now,"\033[0m",end="",sep="")
-            print("\033[33m","="*int(percent*self.PLEN) +"\033[36m" + "_"*(self.PLEN-int(percent*self.PLEN)),"\033[0m ",end="\r",sep="")
+            if self.kstamp+1>tstamp:
+                self.kstamp=tstamp
+                tmt=tstamp-self.t1
+                now=time.localtime(self.t1+tmt*(1/(self.n/self.l)))
+                now = time.strftime("%H:%M \033[0m%d.%m.%Y",now)
+                #print("="*5)
+                print(" \033[34m{:.1%}\033[0m".format(percent), end=" ")
+                print("ETA: %5.1fs\033[0m"%(tmt*(1/(self.n/self.l))-tmt), end=" ",sep="")
+                print("\033[32m",now,"\033[0m",end="",sep="")
+                print("\033[33m","="*int(percent*self.PLEN) +"\033[36m" + "_"*(self.PLEN-int(percent*self.PLEN)),"\033[0m ",end="\r",sep="")
         return (self.n, no)
     
     def __len__(self):
